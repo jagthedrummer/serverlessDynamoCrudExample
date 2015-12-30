1394935b17acc5d5381024b1ad748f674eeeead3
Jeremy Green <jeremy@octolabs.com>
2015-12-30 15:16:30 -0600

    Added a basic index handler

---
+++ back/modules/widgets/lib/dynamo.js
@@ -0,0 +1,16 @@
+var AWS = require('aws-sdk');
+var dynamoconfig = {};
+
+if(process.env.SERVERLESS_STAGE === 'development'){
+  dynamoconfig = {
+    endpoint: "http://localhost:8000",
+    region: "someregion",
+    accessKeyId: "test",
+    secretAccessKey: "test"
+  }
+}
+
+var dynamodb = new AWS.DynamoDB(dynamoconfig);
+module.exports.doc = new AWS.DynamoDB.DocumentClient({service:dynamodb});
+module.exports.tableName = "widgets-" + process.env.SERVERLESS_STAGE;
+
--- back/modules/widgets/lib/index.js
+++ back/modules/widgets/lib/index.js
@@ -2,22 +2,26 @@
  * Lib
  */
 
-module.exports.index = function(event, cb) {
+var dynamo = require('./dynamo');
 
-  var response = {
-    message: "Index ran successfully!",
-    event: event
+module.exports.respond = function(event, cb) {
+
+  var params = {
+    TableName: dynamo.tableName
   };
 
-  return cb(null, response);
+  return dynamo.doc.scan(params, function(err, data) {
+    if (err){
+      console.log(err, err.stack); // an error occurred
+      cb(err);
+    }else{
+      console.log(data);           // successful response
+      var items = data.Items;
+      var newData = {"widgets": items};
+
+      console.log(newData);
+      cb(err,newData);
+    }
+  });
 };
 
-module.exports.show = function(event, cb) {
-
-  var response = {
-    message: "Show ran successfully!",
-    event: event
-  };
-
-  return cb(null, response);
-};
